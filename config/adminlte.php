5ef9c9656b64fcd0fd08a647144e8cf3e64bbb8f
macdiontrue <macamodionisio@gmail.com>
2018-07-06 13:01:51 +0200

    configuracao do admin

--- config/adminlte.php
+++ config/adminlte.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 2',
+    'title' => 'Apadrinhamento',
 
     'title_prefix' => '',
 
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>APC</b>',
 
     'logo_mini' => '<b>A</b>LT',
 
